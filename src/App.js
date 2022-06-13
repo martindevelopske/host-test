d3f4f889a75e28b991f7ed734c12172ed741fac8
martin ndungu <martindevelopske@gmail.com>
2022-06-13 10:55:31 +0300

    ndeged

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn noting 
+          Learn noting and everything 
         </a>
       </header>
     </div>
